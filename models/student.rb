76946e92a8f41e4cfa1f5a0ebeb62c381f45b019
a-greenish-red <a.greenish.red@gmail.com>
2017-07-18 16:22:58 -0500

    simplify score validation, add ruby version

--- models/student.rb
+++ models/student.rb
@@ -7,7 +7,7 @@ class Student
   VALID_SCORES = %w(k 1 2 3 4 5 6 7 8)
 
   def initialize(name, scores = {})
-    raise InvalidTestScore if (scores.values.select{|score| VALID_SCORES.include? score}).empty? && scores != {}
+    scores.values.map{|score| raise InvalidTestScore unless VALID_SCORES.include? score}
 
     @name = name
     @scores = scores
