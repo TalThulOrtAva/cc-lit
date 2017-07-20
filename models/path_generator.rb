9e614a4293b133fefb56c00198424c271843278f
a-greenish-red <a.greenish.red@gmail.com>
2017-07-20 18:41:56 -0500

    cleanup

--- models/path_generator.rb
+++ models/path_generator.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class PathGenerator
   attr_reader :domain_order, :student, :path
 
@@ -18,7 +16,8 @@ class PathGenerator
 
     student.scores.each do |score|
       grade_level = score[1]
-      next if grade_level == 'K'
+
+      next if grade_level == 'K' # a grade of K doesn't alter the lesson order
       max_level_to_delete = score[1].to_i - 1
       learning_path.reject! { |lesson| /([0-#{max_level_to_delete}]|K)\.#{score[0].to_s}/.match(lesson) }
     end
